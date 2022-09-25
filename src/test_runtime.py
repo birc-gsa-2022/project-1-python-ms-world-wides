22903516e7ec1e37829d22fdb213674b52fbbfcb
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-09-25 10:53:15 +0200

    Test runtime file

---
+++ src/test_runtime.py
@@ -0,0 +1,17 @@
+from sequence_generator import genome_sequence_generator
+from lin import
+from naive import 
+
+# test for naive uses no more time than O(nm)
+
+#check 
+# Remember to explain your choice of test data. 
+
+
+# best-case input:  x = A^n y = B^m
+# worst-case input: x = A^n y = A^m
+
+
+# test for lin use no more time than O(n+m)
+# Remember to explain your choice of test data.
+# What are “best” and “worst” case inputs?
